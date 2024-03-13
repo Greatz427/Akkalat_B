f175b9097cb919315a80c8f96313fbf5aa675943
greatz <1284755175@qq.com>
2024-03-13 20:40:50 +0800

    test1

---
+++ Test1/mgpu_config/mesh.go
@@ -0,0 +1,201 @@
+package mgpu_config
+
+import (
+	"fmt"
+
+	"gitlab.com/akita/akita/v2/sim"
+	"gitlab.com/akita/mem/v2/mem"
+	"gitlab.com/akita/mem/v2/vm/mmu"
+	"gitlab.com/akita/mgpusim/v2/driver"
+	"gitlab.com/akita/noc/v2/networking/mesh"
+)
+
+// meshBuild builds a platform with R9Nano GPUs in a mesh
+func (b R9NanoPlatformBuilder) meshBuild() *Platform {
+	engine := b.createEngine()
+	if b.monitor != nil {
+		b.monitor.RegisterEngine(engine)
+	}
+
+	mmuComponent, pageTable := b.createMMU(engine)
+
+	storage := mem.NewStorage(uint64(1+b.numGPU) * 4 * mem.GB)
+
+	gpuDriverBuilder := driver.MakeBuilder()
+	if b.useMagicMemoryCopy {
+		gpuDriverBuilder = gpuDriverBuilder.WithMagicMemoryCopyMiddleware()
+	}
+	gpuDriver := gpuDriverBuilder.
+		WithEngine(engine).
+		WithPageTable(pageTable).
+		WithLog2PageSize(b.log2PageSize).
+		WithGlobalStorage(storage).
+		Build("Driver")
+	// file, err := os.Create("driver_comm.csv")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// gpuDriver.GetPortByName("GPU").AcceptHook(
+	// 	sim.NewPortMsgLogger(log.New(file, "", 0)))
+
+	if b.monitor != nil {
+		b.monitor.RegisterComponent(gpuDriver)
+	}
+
+	gpuBuilder := b.createMeshGPUBuilder(
+		engine, gpuDriver, mmuComponent, storage)
+
+	meshConnector := b.createMeshConnection(engine, gpuDriver, mmuComponent)
+
+	mmuComponent.MigrationServiceProvider = gpuDriver.GetPortByName("MMU")
+
+	rdmaAddressTable := b.createRDMAAddrTable()
+	pmcAddressTable := b.createPMCPageTable()
+
+	b.createMeshGPUs(meshConnector, gpuBuilder, gpuDriver, rdmaAddressTable,
+		pmcAddressTable)
+
+	meshConnector.EstablishNetwork()
+
+	return &Platform{
+		Engine: engine,
+		Driver: gpuDriver,
+		GPUs:   b.gpus,
+	}
+}
+
+func (b *R9NanoPlatformBuilder) createMeshGPUs(
+	meshConnector *mesh.Connector,
+	gpuBuilder R9NanoGPUBuilder,
+	gpuDriver *driver.Driver,
+	rdmaAddressTable *mem.BankedLowModuleFinder,
+	pmcAddressTable *mem.BankedLowModuleFinder,
+) {
+	for z := 0; z < b.meshSize[2]; z++ {
+		for y := 0; y < b.meshSize[1]; y++ {
+			for x := 0; x < b.meshSize[0]; x++ {
+
+				// Reserving (0,0,0) for the CPU
+				//if x != 0 || y != 0 || z != 0 {
+				if !((x == 0) && (y == 0)) { // ||
+					//	((x == 3) && (y == 1)) ||
+					//	((x == 1) && (y == 3)) ||
+					//	((x == 3) && (y == 3))) {
+
+					//i := x + (y * b.meshSize[0]) + (z * b.meshSize[0] * b.meshSize[1])
+					i := int(len(b.gpus)) + 1
+
+					element := [3]int{x, y, z}
+					b.createMeshGPU(i, gpuBuilder, gpuDriver,
+						rdmaAddressTable, pmcAddressTable,
+						meshConnector, element)
+
+					fmt.Println("createMeshGPUs: GPU ", i, element, " GPUs", len(b.gpus))
+					//meshConnector.AddTile([3]int{x, y, z}, b.gpus[i].Domain.Ports())
+					//fmt.Println("createMeshGPUs: GPU ", i, " Ports", b.gpus[i-1].Domain.Ports())
+				}
+			}
+		}
+	}
+}
+
+func (b R9NanoPlatformBuilder) createMeshConnection(
+	engine sim.Engine,
+	gpuDriver *driver.Driver,
+	mmuComponent *mmu.MMU,
+) *mesh.Connector {
+	//connection := sim.NewDirectConnection(engine)
+	// connection := noc.NewFixedBandwidthConnection(32, engine, 1*sim.GHz)
+	// connection.SrcBufferCapacity = 40960000
+	meshConnector := mesh.NewConnector().
+		WithEngine(engine).
+		WithSwitchLatency(10) // Set to 10 ns
+	meshConnector.CreateNetwork("Mesh")
+
+	meshConnector.AddTile([3]int{0, 0, 0},
+		[]sim.Port{
+			gpuDriver.GetPortByName("GPU"),
+			gpuDriver.GetPortByName("MMU"),
+			mmuComponent.GetPortByName("Migration"),
+			mmuComponent.GetPortByName("Top"),
+		})
+	/*meshConnector.AddTile([3]int{3, 1, 0},
+		[]sim.Port{
+			gpuDriver.GetPortByName("GPU"),
+			gpuDriver.GetPortByName("MMU"),
+			mmuComponent.GetPortByName("Migration"),
+			mmuComponent.GetPortByName("Top"),
+		})
+	meshConnector.AddTile([3]int{1, 3, 0},
+		[]sim.Port{
+			gpuDriver.GetPortByName("GPU"),
+			gpuDriver.GetPortByName("MMU"),
+			mmuComponent.GetPortByName("Migration"),
+			mmuComponent.GetPortByName("Top"),
+		})
+	meshConnector.AddTile([3]int{3, 3, 0},
+		[]sim.Port{
+			gpuDriver.GetPortByName("GPU"),
+			gpuDriver.GetPortByName("MMU"),
+			mmuComponent.GetPortByName("Migration"),
+			mmuComponent.GetPortByName("Top"),
+		})*/
+	return meshConnector
+}
+
+func (b *R9NanoPlatformBuilder) createMeshGPUBuilder(
+	engine sim.Engine,
+	gpuDriver *driver.Driver,
+	mmuComponent *mmu.MMU,
+	storage *mem.Storage,
+) R9NanoGPUBuilder {
+	gpuBuilder := MakeR9NanoGPUBuilder().
+		WithEngine(engine).
+		WithMMU(mmuComponent).
+		WithNumCUPerShaderArray(4).
+		WithNumShaderArray(4).
+		WithNumMemoryBank(1).
+		WithLog2MemoryBankInterleavingSize(7).
+		WithLog2PageSize(b.log2PageSize).
+		WithGlobalStorage(storage)
+
+	if b.monitor != nil {
+		gpuBuilder = gpuBuilder.WithMonitor(b.monitor)
+	}
+
+	gpuBuilder = b.setVisTracer(gpuDriver, gpuBuilder)
+	gpuBuilder = b.setMemTracer(gpuBuilder)
+	gpuBuilder = b.setISADebugger(gpuBuilder)
+
+	return gpuBuilder
+}
+
+func (b *R9NanoPlatformBuilder) createMeshGPU(
+	index int,
+	gpuBuilder R9NanoGPUBuilder,
+	gpuDriver *driver.Driver,
+	rdmaAddressTable *mem.BankedLowModuleFinder,
+	pmcAddressTable *mem.BankedLowModuleFinder,
+	meshConnector *mesh.Connector,
+	element [3]int,
+) *GPU {
+	name := fmt.Sprintf("GPU%d", index)
+	memAddrOffset := uint64(index) * 4 * mem.GB
+	gpu := gpuBuilder.
+		WithMemAddrOffset(memAddrOffset).
+		Build(name, uint64(index))
+
+	//fmt.Println("createMeshGPU: Mem Addr Offset ", index, memAddrOffset)
+	gpuDriver.RegisterGPU(gpu.Domain.GetPortByName("CommandProcessor"),
+		4*mem.GB)
+	gpu.CommandProcessor.Driver = gpuDriver.GetPortByName("GPU")
+
+	b.configRDMAEngine(gpu, rdmaAddressTable)
+	b.configPMC(gpu, gpuDriver, pmcAddressTable)
+
+	meshConnector.AddTile(element, gpu.Domain.Ports())
+
+	b.gpus = append(b.gpus, gpu)
+
+	return gpu
+}
---
+++ go.mod
@@ -0,0 +1,25 @@
+module github.com/Greatz427/Akkalat_B/Test1
+
+go 1.22.1
+
+require gitlab.com/akita/mem/v2 v2.5.0
+
+require (
+	github.com/aws/aws-sdk-go v1.29.15 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.2 // indirect
+	github.com/jmespath/go-jmespath v0.0.0-20180206201540-c2b33e8439af // indirect
+	github.com/klauspost/compress v1.9.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rs/xid v1.2.1 // indirect
+	github.com/tebeka/atexit v0.3.0 // indirect
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v1.0.0 // indirect
+	gitlab.com/akita/akita/v2 v2.0.0-alpha.3 // indirect
+	gitlab.com/akita/util/v2 v2.0.0-alpha.3 // indirect
+	go.mongodb.org/mongo-driver v1.4.1 // indirect
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
+	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9 // indirect
+	golang.org/x/text v0.3.3 // indirect
+)
